481c0d8342b0c3f10f045691a79b7503b0e7b241
Islam Ahmad <islam.ahmad@almtar.com>
2019-02-03 14:21:04 +0200

    created view with issues

--- lib/util/notodo_screen.dart
+++ lib/util/notodo_screen.dart
@@ -6,6 +6,7 @@ class NoTodoScreen extends StatefulWidget {
 }
 
 class _NoTodoScreenState extends State<NoTodoScreen> {
+  final TextEditingController _tec = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,23 +23,43 @@ class _NoTodoScreenState extends State<NoTodoScreen> {
         backgroundColor: Colors.redAccent,
         foregroundColor: Colors.white,
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            color: Colors.black45,
-            child: ListTile(),
-            elevation: 3.0,
-          ),
-          Padding(padding: EdgeInsets.all(5.0)),
-          Card(
-            child: ListTile(),
-            color: Colors.black45,
-            elevation: 3.0,
-          ),
-        ],
-      ),
+      body: Column(),
     );
   }
 
-  _showFormDialog() {}
+  _showFormDialog() {
+    var alert = AlertDialog(
+      content: Row(
+        children: <Widget>[
+          Expanded(
+              child: TextField(
+            controller: _tec,
+            autocorrect: true,
+            decoration: InputDecoration(
+              labelText: 'Item',
+              hintText: 'e.g. dont buy stuff',
+              icon: Icon(Icons.save),
+            ),
+          ))
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () => _handleSubmit(_tec.text),
+          child: Icon(Icons.save_alt),
+        ),
+        FlatButton(
+          onPressed: () => Navigator.pop(context),
+          child: Icon(Icons.cancel),
+        )
+      ],
+    );
+    showDialog(
+        context: context,
+        builder: (_) {
+          return alert;
+        });
+  }
+
+  _handleSubmit(String text) async {}
 }
